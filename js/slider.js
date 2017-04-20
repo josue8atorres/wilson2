c08ac23757d306db104d23b871d6d4dfc6cf2d50
josue8atorres <josue8a7@gmail.com>
2017-04-20 10:41:58 -0500

    actualizacion
    actualización

---
+++ js/slider.js
@@ -0,0 +1,16 @@
+var actual=1;
+$(document).on("ready",main);
+
+function main () {
+	var intervalo=setInterval("runSlider()",5000);
+}
+
+function runSlider () {
+	if (actual==$("#containerSlider img").size()) {
+		actual=0;
+	}
+	$("#containerSlider").animate({
+		marginLeft:(-1*actual*$("#containerSlider img").eq(0).width())
+	},2000);
+	actual++;
+}
